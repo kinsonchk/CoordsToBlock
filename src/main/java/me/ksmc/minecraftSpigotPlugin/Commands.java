eaa265b26434bec740eed483b05668eacdbf7bf6
Kinson <95203053+kinsonchk@users.noreply.github.com>
2026-06-07 17:40:33 -0400

    Update

---
+++ src/main/java/me/ksmc/minecraftSpigotPlugin/Commands.java
@@ -0,0 +1,14 @@
+package me.ksmc.minecraftSpigotPlugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Commands implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        return true;
+    }
+}
--- src/main/java/me/ksmc/minecraftSpigotPlugin/MinecraftSpigotPlugin.java
+++ src/main/java/me/ksmc/minecraftSpigotPlugin/MinecraftSpigotPlugin.java
@@ -7,10 +7,16 @@ public final class MinecraftSpigotPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        System.out.println("[SpigotPlugin] Plugin has been enabled!");
+
+        getCommand("ctb").setExecutor(new Commands());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+
+        System.out.println("[SpigotPlugin] Plugin has been disabled!");
     }
 }
