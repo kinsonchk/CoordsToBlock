c23910a635d16c680f1ac7cff78a9ad997dfc3e2
Kinson <95203053+kinsonchk@users.noreply.github.com>
2026-06-07 15:52:59 -0400

    Repo setup

---
+++ src/main/java/me/ksmc/minecraftSpigotPlugin/MinecraftSpigotPlugin.java
@@ -0,0 +1,16 @@
+package me.ksmc.minecraftSpigotPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MinecraftSpigotPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
