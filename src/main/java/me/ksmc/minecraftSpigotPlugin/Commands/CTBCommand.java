8b22a7f34dc1c6d5b3ca1dd8279f43ad121b03eb
Kinson <95203053+kinsonchk@users.noreply.github.com>
2026-06-12 02:32:51 -0400

    Change float to double for more precision

--- src/main/java/me/ksmc/minecraftSpigotPlugin/Commands/CTBCommand.java
+++ src/main/java/me/ksmc/minecraftSpigotPlugin/Commands/CTBCommand.java
@@ -15,48 +15,48 @@ public class CTBCommand implements CommandExecutor {
             Player p = (Player) sender;
 
             if (args.length != 10) {
-                p.sendMessage(ChatColor.RED + "Error: Incorrect command usage.\n");
+                p.sendMessage(ChatColor.RED + "Error: Incorrect command usage.");
                 return false;
             }
 
             // Initiate variables
             // Real-world coordinate boundaries (LAT/LONG)
-            float westLong = 0;
-            float southLat = 0;
-            float eastLong = 0;
-            float northLat = 0;
+            double westLong = 0;
+            double southLat = 0;
+            double eastLong = 0;
+            double northLat = 0;
 
             // Minecraft world's boundary positions (X/Z)
-            float topLeftX = 0;
-            float topLeftZ = 0;
-            float bottomRightX = 0;
-            float bottomRightZ = 0;
+            double topLeftX = 0;
+            double topLeftZ = 0;
+            double bottomRightX = 0;
+            double bottomRightZ = 0;
 
             // Target pos in real-world coords to convert (LAT/LONG)
-            float targetLat = 0;
-            float targetLong = 0;
+            double targetLat = 0;
+            double targetLong = 0;
 
             // Scales for calculation
-            float ewScale = 0;
-            float nsScale = 0;
+            double ewScale = 0;
+            double nsScale = 0;
 
             // Final result
-            float resultX = 0;
-            float resultZ = 0;
+            double resultX = 0;
+            double resultZ = 0;
 
             try {
-                westLong = Float.parseFloat(args[0]);
-                southLat = Float.parseFloat(args[1]);
-                eastLong = Float.parseFloat(args[2]);
-                northLat = Float.parseFloat(args[3]);
+                westLong = Double.parseDouble(args[0]);
+                southLat = Double.parseDouble(args[1]);
+                eastLong = Double.parseDouble(args[2]);
+                northLat = Double.parseDouble(args[3]);
 
-                topLeftX = Float.parseFloat(args[4]);
-                topLeftZ = Float.parseFloat(args[5]);
-                bottomRightX = Float.parseFloat(args[6]);
-                bottomRightZ = Float.parseFloat(args[7]);
+                topLeftX = Double.parseDouble(args[4]);
+                topLeftZ = Double.parseDouble(args[5]);
+                bottomRightX = Double.parseDouble(args[6]);
+                bottomRightZ = Double.parseDouble(args[7]);
 
-                targetLat = Float.parseFloat(args[8]);
-                targetLong = Float.parseFloat(args[9]);
+                targetLat = Double.parseDouble(args[8]);
+                targetLong = Double.parseDouble(args[9]);
             } catch (NumberFormatException e) {
                 p.sendMessage(ChatColor.RED + "Error: Only valid numbers are allowed as arguments.");
                 return false;
@@ -74,7 +74,7 @@ public class CTBCommand implements CommandExecutor {
             resultZ = nsScale * (northLat - targetLat) + topLeftZ;
 
             p.sendMessage(ChatColor.GREEN + "CONVERTED BLOCK POSITION (X, Z): " +
-                    ChatColor.ITALIC + Math.round(resultX) + ", " + Math.round(resultZ));
+                    ChatColor.UNDERLINE + Math.round(resultX) + ", " + Math.round(resultZ));
 
         } else {
             sender.sendMessage(ChatColor.RED + "Only players can execute this command.");
